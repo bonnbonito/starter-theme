a4c2c6962013a9b25475ff02f07f56f03c08992b
unknown <bonnbonito@gmail.com>
2017-04-03 22:31:23 +0800

    Fixed convert_smilies on 404

--- 404.php
+++ 404.php
@@ -48,7 +48,7 @@ get_header(); ?>
 						endif;
 
 						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'bonnjoel' ), convertbonnjoelmilies( ':)' ) ) . '</p>';
+						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'bonnjoel' ), convert_smilies( ':)' ) ) . '</p>';
 						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
 
 						the_widget( 'WP_Widget_Tag_Cloud' );
