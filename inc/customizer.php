9ed10cea3b311b21c94908a51037c22be98edd90
unknown <bonnbonito@gmail.com>
2017-04-06 16:47:32 +0800

    Fix customizer error

--- inc/customizer.php
+++ inc/customizer.php
@@ -11,9 +11,9 @@
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
 function bonnjoel_customize_register( $wp_customize ) {
-	$wp_customize->getbonnjoeletting( 'blogname' )->transport         = 'postMessage';
-	$wp_customize->getbonnjoeletting( 'blogdescription' )->transport  = 'postMessage';
-	$wp_customize->getbonnjoeletting( 'header_textcolor' )->transport = 'postMessage';
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
 }
 add_action( 'customize_register', 'bonnjoel_customize_register' );
 
