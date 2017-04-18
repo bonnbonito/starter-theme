b4de0cda03b5c6925e8f37119fb12a95b63723c4
unknown <bonnbonito@gmail.com>
2017-04-18 21:08:39 +0800

    Added jssocials

--- custom_functions.php
+++ custom_functions.php
@@ -15,12 +15,15 @@ function bonnjoel_addmore_scripts() {
 	wp_enqueue_style( 'bonnjoel_mmenu-css', get_template_directory_uri() . '/css/mmenu.css' );
 	wp_enqueue_style( 'bonnjoel_lightbox-css', get_template_directory_uri() . '/css/lightbox.css' );
 	wp_enqueue_style( 'bonnjoel_custom-css', get_template_directory_uri() . '/custom.css' );
+	wp_register_style( 'bonnjoel_jsscocials_style', '//cdn.jsdelivr.net/jquery.jssocials/1.4.0/jssocials.css' );
+	wp_register_style( 'bonnjoel_jsscocials_style_flat', '//cdn.jsdelivr.net/jquery.jssocials/1.4.0/jssocials-theme-flat.css' );
 
 	wp_register_script( 'bonnjoel_mmenu-js', get_template_directory_uri() . '/js/mmenu.min.js', array('jquery'), 'v5.6.5', true );
 	wp_register_script( 'bonnjoel_lightbox-js', get_template_directory_uri() . '/js/lightbox.js', 'jquery', 'v2.8.2', true );
 	wp_register_script( 'bonnjoel_imgloaded-js', get_template_directory_uri() . '/js/imagesloaded.min.js', '', 'v4.1.0', true );	
 	wp_register_script( 'bonnjoel_fastclick', get_template_directory_uri() . '/js/fastclick.js', '', '1.0.6' );
 	wp_register_script( 'bonnjoel_nicescroll', get_template_directory_uri() . '/js/jquery.nicescroll.min.js', array('jquery'), false, true );
+	wp_register_script( 'bonnjoel_jssocials', '//cdn.jsdelivr.net/jquery.jssocials/1.4.0/jssocials.min.js', array('jquery'), '1.4.0', false );
 
 	wp_enqueue_script( 'bonnjoel_imgloaded-js' );
 	wp_enqueue_script( 'bonnjoel_fastclick' );
