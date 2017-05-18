0f6b2ea90931990ea539c8ed1010797bab171e3c
unknown <bonnbonito@gmail.com>
2017-05-18 20:45:49 +0800

    Removed Bootsrap, Add Semantic UI

--- custom_functions.php
+++ custom_functions.php
@@ -6,15 +6,25 @@
  *
  * @package BonnJoel
  */
+
+remove_filter( 'the_content', 'wpautop' );
+remove_filter( 'the_excerpt', 'wpautop' );
+
 function bonnjoel_addmore_scripts() {
 	/*
 	 * Enqueue custom scripts and styles	 
 	 */
-	wp_enqueue_style( 'bonnjoel_bootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css' );
-	wp_enqueue_style( 'bonnjoel_fontawesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css' );	
+
+	if (!is_admin()) {
+		wp_deregister_script('jquery');
+		wp_register_script('jquery', '//code.jquery.com/jquery-3.1.1.min.js', false, '3.1.1');
+		wp_enqueue_script('jquery');
+	}
+
+	wp_enqueue_style( 'bonnjoel_semantic-css', get_template_directory_uri() . '/semantic/dist/semantic.min.css' );
 	wp_enqueue_style( 'bonnjoel_mmenu-css', get_template_directory_uri() . '/css/mmenu.css' );
 	wp_enqueue_style( 'bonnjoel_lightbox-css', get_template_directory_uri() . '/css/lightbox.css' );
-	wp_enqueue_style( 'bonnjoel_custom-css', get_template_directory_uri() . '/custom.css' );
+	wp_enqueue_style( 'bonnjoel-style', get_stylesheet_uri() );
 	wp_register_style( 'bonnjoel_jsscocials_style', '//cdn.jsdelivr.net/jquery.jssocials/1.4.0/jssocials.css' );
 	wp_register_style( 'bonnjoel_jsscocials_style_flat', '//cdn.jsdelivr.net/jquery.jssocials/1.4.0/jssocials-theme-flat.css' );
 
@@ -25,12 +35,13 @@ function bonnjoel_addmore_scripts() {
 	wp_register_script( 'bonnjoel_nicescroll', get_template_directory_uri() . '/js/jquery.nicescroll.min.js', array('jquery'), false, true );
 	wp_register_script( 'bonnjoel_jssocials', '//cdn.jsdelivr.net/jquery.jssocials/1.4.0/jssocials.min.js', array('jquery'), '1.4.0', false );
 
+	wp_enqueue_script( 'bonnjoel_semantic-js', get_template_directory_uri() . '/semantic/dist/semantic.min.js', array('jquery'), '2.2.10', true );
+
 	wp_enqueue_script( 'bonnjoel_imgloaded-js' );
 	wp_enqueue_script( 'bonnjoel_fastclick' );
 	wp_enqueue_script( 'bonnjoel_mmenu-js' );
 	wp_enqueue_script( 'bonnjoel_nicescroll' );
 	wp_enqueue_script( 'bonnjoel_tether', '//cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js', array('jquery'), '1.4.0', true );
-	wp_enqueue_script( 'bonnjoel_bootstrapjs', '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js', array('jquery', 'bonnjoel_tether'), '0.0.6', true );	
 	wp_enqueue_script( 'bonnjoel_theme-js', get_template_directory_uri() . '/js/theme.js', array('jquery','bonnjoel_mmenu-js', 'bonnjoel_nicescroll', 'bonnjoel_imgloaded-js'), false, true );	
 	
 }
