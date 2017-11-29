529ae9f33055af5014d98b8b2ad4a148f3112506
unknown <bonnbonito@gmail.com>
2017-11-29 17:31:20 +0800

    Added images and fix simple dropdown on menu

--- inc/walker.php
+++ inc/walker.php
@@ -1,31 +1,31 @@
 <?php
-	
+
 /* Collection of Walker classes */
 
 	/*
-		
+
 		wp_nav_menu()
-		
+
 		<div class="menu-container">
 			<ul> // start_lvl()
 				<li><a><span> // start_el()
-				
+
 					</a></span>
-					
+
 					<ul>
 					</li> // end_el()
-					
+
 				<li><a>Link</a></li>
 				<li><a>Link</a></li>
 				<li><a>Link</a></li>
-				
+
 			</ul> // end_lvl()
 		</div>
-		
+
 	*/
-	
+
 class Walker_Nav_Primary extends Walker_Nav_menu {
-	
+
 	/**
 	 * Starts the list before the elements are added.
 	 *
@@ -39,7 +39,7 @@ class Walker_Nav_Primary extends Walker_Nav_menu {
 		$indent  = str_repeat("\t", $depth);
 		$output .= "\n$indent<i class=\"dropdown icon\"></i><span class=\"menu\">\n";
 	}
-	
+
 	/**
 	 * Ends the list of after the elements are added.
 	 *
@@ -53,7 +53,7 @@ class Walker_Nav_Primary extends Walker_Nav_menu {
 		$indent  = str_repeat("\t", $depth);
 		$output .= "$indent</span>\n";
 	}
-	
+
 	/**
 	 * Start the element output.
 	 *
@@ -67,30 +67,30 @@ class Walker_Nav_Primary extends Walker_Nav_menu {
 	 */
 	public function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0) {
 		$indent = ($depth) ? str_repeat("\t", $depth) : '';
-		
+
 		$classes   = empty($item->classes) ? array() : (array) $item->classes;
 		if (in_array('menu-item-has-children', $classes)) {
-			$classes[] = 'ui dropdown';
+			$classes[] = 'ui dropdown simple';
 		}
 		$classes[] = 'item';
 		$classes[] = 'menu-item-' . $item->ID;
-		
+
 		$class_names = join(' ', apply_filters('nav_menu_css_class', array_filter($classes), $item, $args, $depth));
 		$class_names = $class_names ? ' class="' . esc_attr($class_names) . '"' : '';
-		
+
 		$id = apply_filters('nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args, $depth);
 		$id = $id ? ' id="' . esc_attr($id) . '"' : '';
-		
+
 		$output .= $indent . '<span' . $id . $class_names .'>';
-		
+
 		$atts           = array();
 		$atts['title']  = !empty($item->attr_title) ? $item->attr_title : '';
 		$atts['target'] = !empty($item->target)     ? $item->target     : '';
 		$atts['rel']    = !empty($item->xfn)        ? $item->xfn        : '';
 		$atts['href']   = !empty($item->url)        ? $item->url        : '';
-		
+
 		$atts = apply_filters('nav_menu_link_attributes', $atts, $item, $args, $depth);
-		
+
 		$attributes = '';
 		foreach ($atts as $attr => $value) {
 			if (! empty($value)) {
@@ -98,17 +98,17 @@ class Walker_Nav_Primary extends Walker_Nav_menu {
 				$attributes .= ' ' . $attr . '="' . $value . '"';
 			}
 		}
-		
+
 		$item_output = $args->before;
 		$item_output .= '<a'. $attributes .'>';
 		/** This filter is documented in wp-includes/post-template.php */
 		$item_output .= $args->link_before . apply_filters('the_title', $item->title, $item->ID) . $args->link_after;
 		$item_output .= '</a>';
 		$item_output .= $args->after;
-		
+
 		$output .= apply_filters('walker_nav_menu_start_el', $item_output, $item, $depth, $args);
 	}
-	
+
 	/**
 	 * Ends the element output, if needed.
 	 *
@@ -124,5 +124,5 @@ class Walker_Nav_Primary extends Walker_Nav_menu {
 		// $output .= $args->after;
 		$output .= "</span>\n";
 	}
-	
+
 }
